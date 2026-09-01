be7cdf933d155300c94b5fe87a25ceadd80c688d
Rayen B <ra.beltran@duocuc.cl>
2026-09-01 17:55:12 -0400

    inicio

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
-function inyectarHeader(){
-
+function inyectarFooter(){
+    document.getElementById("footer").innerHTML = "<p>web peich meiqued bai gueimers</p>"
 }
 
-function inyectarFooter(){
+function inyectarHeader(){
+    document.getElementById("header").innerHTML = "<section><h1 class='no-select'>plei videgueims</h1></section><section><a href='#inicio'>inicio</a><a href='#info'>info</a><a href='#contacto'>contacto</a></section><section><p class='no-select'>jaf fan</p></section>";
     
 }
+
+inyectarHeader();
+inyectarFooter();
