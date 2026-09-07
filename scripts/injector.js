e745354740304aa433c54c3327cc20918bc45ceb
Thomas <th.mery@duocuc.cl>
2026-09-07 19:26:03 -0300

    Update 2
    creado script injector para que todas las paginas puedan acceder a los headers y footers.

---
+++ scripts/injector.js
@@ -0,0 +1,12 @@
+function inyectarHeader(){
+  document.getElementById("header").innerHTML = '<img href="/index.html" src="https://yt3.googleusercontent.com/ytc/AIdro_lUdOZuGBEOWyjiFl4M273jIjl_LObZoJJlVqd5mYdp4vA=s900-c-k-c0x00ffffff-no-rj" alt="Logo Veterinaria" class="logo"><h1 class= "no-select"> Veterinaria Huellitas</h1><section> <a href="index.html">Inicio</a> <a href="paginas/nosotros.html">Nosotros</a> <a href="paginas/productos.html">Productos</a> <a href="paginas/blogs.html">Blog</a></section>';
+
+}
+
+function inyectarFooter(){
+    document.getElementById("footer").innerHTML = '<div class="caluga"> <p>     Veterinaria Huellitas - Cuidando pokemones desde 1996 </p> <p>     © 2026 Veterinaria Huellitas. Todos los derechos reservados. </p> </div>';
+}
+
+
+inyectarHeader();
+inyectarFooter();
