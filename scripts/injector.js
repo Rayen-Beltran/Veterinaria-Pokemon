7290c3802cea1e72d13fb1a84015ac4a4dfadcd7
Thomas <th.mery@duocuc.cl>
2026-09-07 22:22:34 -0300

    arreglos en el injector pt10

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,6 +1,23 @@
-function inyectarHeader(){
-  document.getElementById("header").innerHTML = '<a href="Veterinaria-Pokemon/index.html"> <img src="Veterinaria-Pokemon/imagenes/logoPoke" alt="Logo Veterinaria" class="logo"> </a> <h1 class= "no-select"> Veterinaria Huellitas</h1><section> <a href="Veterinaria-Pokemon/index.html">Inicio</a> <a href="Veterinaria-Pokemon/paginas/nosotros.html">Nosotros</a> <a href="Veterinaria-Pokemon/paginas/productos.html">Productos</a> <a href="Veterinaria-Pokemon/paginas/blogs.html">Blog</a></section> <section> <a href="Veterinaria-Pokemon/paginas/registroEntrenador.html">Registrate</a> <a href="Veterinaria-Pokemon/paginas/inicioSesion.html">Iniciar Sesion</a> </section>';
+const scriptSrc = document.currentScript.src;
+const basePath = scriptSrc.substring(0, scriptSrc.indexOf('scripts/'));
 
+function inyectarHeader(){
+  document.getElementById("header").innerHTML = `
+    <a href="${basePath}index.html">
+      <img src="https://yt3.googleusercontent.com/ytc/AIdro_lUdOZuGBEOWyjiFl4M273jIjl_LObZoJJlVqd5mYdp4vA=s900-c-k-c0x00ffffff-no-rj" alt="Logo Veterinaria" class="logo">
+    </a>
+    <h1 class="no-select">Veterinaria Huellitas</h1>
+    <section>
+      <a href="${basePath}index.html">Inicio</a>
+      <a href="${basePath}paginas/nosotros.html">Nosotros</a>
+      <a href="${basePath}paginas/productos.html">Productos</a>
+      <a href="${basePath}paginas/blogs.html">Blog</a>
+    </section>
+    <section>
+      <a href="${basePath}paginas/registroEntrenador.html">Registrate</a>
+      <a href="${basePath}paginas/inicioSesion.html">Iniciar Sesion</a>
+    </section>
+  `;
 }
 
 function inyectarFooter(){
