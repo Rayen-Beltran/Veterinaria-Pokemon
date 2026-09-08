024e4e1e3fd145f45d35225b1c904207e15d913e
Thomas <th.mery@duocuc.cl>
2026-09-07 23:03:24 -0300

    arreglo de logo

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,7 +1,7 @@
 function inyectarHeader(){
   document.getElementById("header").innerHTML = `
     <a href="https://rayen-beltran.github.io/Veterinaria-Pokemon/index.htm">
-      <img src="https://rayen-beltran.github.io/Veterinaria-Pokemon/imagenes/logoPoke" alt="Logo Veterinaria" class="logo">
+      <img src="https://rayen-beltran.github.io/Veterinaria-Pokemon/imagenes/logoPoke.webp" alt="Logo Veterinaria" class="logo">
     </a>
     <h1 class="no-select">Veterinaria Huellitas</h1>
     <section>
