90ed487d1f012b68d59af7cf28a194abf9cb7dac
Thomas <th.mery@duocuc.cl>
2026-09-08 03:31:33 -0300

    registro y validadores.

---
+++ scripts/registro.js
@@ -0,0 +1,46 @@
+document.getElementById("form-registro").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let valido = true;
+
+  valido = validarCampo(
+    "nombre",
+    contenido => contenido.trim().length > 0,
+    "El nombre es obligatorio."
+  ) && valido;
+
+  valido = validarCampo(
+    "correo",
+    validarCorreo,
+    "el correo es obligatorio."
+  ) && valido;
+
+  valido = validarCampo(
+    "correo2",
+    contenido => contenido === document.getElementById("correo").value,
+    "Los correos no coinciden."
+  ) && valido;
+
+  valido = validarCampo(
+    "contraseña",
+    contenido => contenido.length >= 4 && contenido.length <= 10,
+    "La contraseña debe tener entre 4 y 10 caracteres."
+  ) && valido;
+
+  valido = validarCampo(
+    "contraseña2",
+    contenido => contenido === document.getElementById("contrasena").value,
+    "Las contraseñas no coinciden."
+  ) && valido;
+
+  valido = validarCampo(
+    "region",
+    contenido => contenido !== "",
+    "Debe seleccionar una región."
+  ) && valido;
+
+  if (valido) {
+    alert(`¡Bienvenido a Huellitas, ${document.getElementById("nombre")}!`);
+    this.reset();
+    document.getElementById("region").innerHTML = '<option value="">-- Seleccione su conjunto residencial --</option>';
+  }
+})
